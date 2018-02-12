902aff65f1eb91081f429b9d26b40b5a8048897c
Jose Carlos <tallafoc@Joses-Mac.local>
2018-02-12 10:35:18 -0800

    - Added sorting implementation - Due to the lack of "last added" value on Tutor object, it was changed to inverse alphabetical

--- Objects/Sorting.swift
+++ Objects/Sorting.swift
@@ -11,25 +11,36 @@ import Foundation
 enum Sorting{
     case alph
     case rating
-    case lastadded
+    case inversealph
     case lastnamealph
     case firstwithphoto
 }
 
 public class Sorted{
-    class func sort(by: Sorting, tutors: [Tutor]){
-        switch(by){
-        case .alph:
-            print("")
-        case .rating:
-            print("")
-        case .lastadded:
-            print("")
-        case .lastnamealph:
-            print("")
-        case .firstwithphoto:
-            print("")
-            //TODO: And so on...
+    class func sort(by: Sorting, tutors: [Tutor]) -> [Tutor]{
+        let ret = tutors.sorted {
+            switch(by){
+            case .alph:
+                return $0.name < $1.name
+            case .rating:
+                return $0.rating > $1.rating
+            case .inversealph:
+                return $0.name < $1.name
+            case .lastnamealph:
+                let name0 = $0.name.split(separator: ",")[1]
+                let name1 = $1.name.split(separator: ",")[1]
+                return name0 < name1
+            case .firstwithphoto:
+                let thereisphoto0 = $0.photo != nil
+                let thereisphoto1 = $1.photo != nil
+                if (thereisphoto1 && !thereisphoto0){
+                    return false
+                }
+                else{
+                    return true
+                }
+            }
         }
+        return ret
     }
 }
