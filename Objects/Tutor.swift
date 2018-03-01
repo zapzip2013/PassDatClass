5bcbf6e6eaf574c77b9e35f6ee4832cc8dd1b88c
Jose Carlos <tallafoc@Joses-Mac.local>
2018-03-01 12:31:08 -0800

    - Added final design for tutor - Added methods to login and passwords - Maybe Course and User is no longer needed?

--- Objects/Tutor.swift
+++ Objects/Tutor.swift
@@ -13,47 +13,69 @@ public class Tutor : User {
     //MARK: Properties
     public var phone : Int
     public var email: String
-    public var name: String
-    public var rating: Int
+    public var firstname: String
+    public var lastname: String
+    public var rating: Float
+    public var numbervotes: Int
     public var photo: UIImage?
-    public var code: Int?
+    public var priceperhour: Float
     public var fsuverified: Bool
     public var bio: String
-    
-    //MARK: Initializers
-    public init!(phone: Int, email: String, name: String, rating: Int, photo: UIImage? = nil, verified: Bool, bio: String){
-        self.phone = phone
-        self.email = email
-        self.name = name
-        self.rating = rating
-        self.photo = photo
-        self.fsuverified = verified
-        self.bio = bio
+    public var name : String {
+        get{
+            return lastname + "," + firstname
+        }
     }
     
-    public init(email: String){
+    //MARK: Initializers
+    public init(phone: Int, email: String, name: String, lastname: String, rating: Float, numbervotes: Int, photo: UIImage? = nil, price: Float, verified: Bool, bio: String){
+        self.phone = phone
         self.email = email
-        //TODO: Retrieve from DB using SQLInteract
-        self.phone = 0
-        self.name = ""
-        self.rating = 0
-        self.photo = nil
-        self.fsuverified = false
-        self.bio = ""
+        self.firstname = name
+        self.lastname = lastname
+        self.rating = rating
+        self.numbervotes = numbervotes
+        self.photo = photo
+        self.priceperhour = price
+        self.fsuverified = verified
+        self.bio = bio
     }
     
     //MARK: Methods
     class func ParseResult(_ row: RowQuery) -> Tutor {
         let firstname = row["firstName"] as! String
         let lastname = row["lastName"] as! String
-        //let priceperhour = row["pricePerHour"] as! Double
+        let priceperhour = row["pricePerHour"] as! Float
+        let numbervotes = row["numbervotes"] as! Int
         let phone = row["phoneNumber"] as! Int
         let bio = row["bio"] as! String
-        let rating = row["rating"] as! Int
+        let rating = row["rating"] as! Float
         let fsuverified = row["fsuverified"] as! Bool
         let email = row["email"] as! String
-        let ret = Tutor(phone: phone, email: email, name: lastname+","+firstname, rating: rating, verified: fsuverified, bio: bio)
-        return ret!
+        let photo = row["photo"] as? UIImage
+        let ret = Tutor(phone: phone, email: email, name: firstname, lastname: lastname, rating: rating, numbervotes: numbervotes, photo: photo, price: priceperhour, verified: fsuverified, bio: bio)
+        return ret
+    }
+    
+    class func encrypt(_ pass: String) -> String {
+        // TODO
+        return pass
+    }
+    
+    class func AddSaltPepper(pass: String) -> String {
+        let salt = "DDSADSA;"
+        let pepper = "435432!"
+        let result = salt + pass + pepper;
+        let cryptresult = encrypt(result)
+        return cryptresult
+    }
+    
+    class func LogIn(email: String, password: String) -> Bool {
+        let query = "SELECT * FROM users WHERE email='" + email + "';"
+        let resquery = SQLInteract.ExecuteSelect(query: query)
+        let DBpass = resquery.first!["password"] as! String
+        let cryptpass = AddSaltPepper(pass: password)
+        return cryptpass == DBpass
     }
     
     class func QueryAccount(email: String) -> Tutor {
@@ -76,6 +98,7 @@ public class Tutor : User {
     
     class func CreateAccount(tutor: Tutor) -> (Bool,String) {
         let query = "INSERT INTO tutors VALUES (...);"
+        //TODO: TABLE USERS? DOUBT
         return SQLInteract.ExecuteModification(query: query)
     }
     
