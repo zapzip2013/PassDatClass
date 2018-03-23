228927e07c8da5f0257901e23ad6b834f4618ecc
Bradley <bhc14@my.fsu.edu>
2018-03-23 09:19:08 -0400

    Clean up code

---
+++ PassDatClass/SignUpViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SignUpViewController.swift
+//  
+//
+//  Created by Bradley Close on 3/21/18.
+//
+
+import Foundation
+import UIKit
+class SignUpViewController:UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var dismissButton: UIButton!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var tapToChangeProfileButton: UIButton!
+    
+    var continueButton:RoundedWhiteButton!
+    var activityView:UIActivityIndicatorView!
+    
+    var imagePicker:UIImagePickerController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
+        
+        continueButton = RoundedWhiteButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        continueButton.setTitleColor(secondaryColor, for: .normal)
+        continueButton.setTitle("Continue", for: .normal)
+        continueButton.titleLabel?.font = UIFont.systemFont(ofSize: 18.0, weight: UIFont.Weight.bold)
+        continueButton.center = CGPoint(x: view.center.x, y: view.frame.height - continueButton.frame.height - 24)
+        continueButton.highlightedColor = UIColor(white: 1.0, alpha: 1.0)
+        continueButton.defaultColor = UIColor.white
+       // continueButton.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
+    }
+}
