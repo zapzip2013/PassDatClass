c4753aac8dc0d667cf7e0aaa48489da84b3098f1
Jose Carlos <jctq1@alu.ua.es>
2018-04-01 19:31:13 -0400

    - Sign UP merged to the project

---
+++ PassDatClass/GenericModalViewController.swift
@@ -0,0 +1,38 @@
+//
+//  GenericModalViewController.swift
+//  PassDatClass
+//
+//  Created by Tallafoc on 4/1/18.
+//  Copyright © 2018 Daniel Gibney. All rights reserved.
+//
+
+import UIKit
+
+class GenericModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func btnClicked(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
